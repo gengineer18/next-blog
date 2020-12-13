eec3b324d1eed746e4626914de090317d7b52282
妹尾弦 <gengineer.web@gmail.com>
2020-12-13 20:15:31 +0900

    🎉 first commit!

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          <p>test</p>
+        </div>
+      </main>
+    </div>
+  )
+}
