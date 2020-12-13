37b6ac06aa1351e54148362c4bd19e1b30db73ab
妹尾弦 <gengineer.web@gmail.com>
2020-12-13 20:57:46 +0900

    🔧 ESLint設定の修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { ChakraProvider } from "@chakra-ui/react"
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: any) {
   return (
