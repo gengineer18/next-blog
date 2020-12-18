47efaf601a1e1a2756ab68d077c6afacea47c516
妹尾弦 <gengineer.web@gmail.com>
2020-12-18 22:14:34 +0900

    ✨ Gridでレイアウトを決める

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react'
 import { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Grid, GridItem, Container } from '@chakra-ui/react'
 import { TheHeader } from '@/components/common/organisms'
+import { baseW } from '@/utils/common'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
       <TheHeader />
-      <Component {...pageProps} />
+      <Container maxW={baseW}>
+        <Grid templateColumns='repeat(8, 1fr)' gap={4}>
+          <GridItem colSpan={6}>
+            <main style={{ border: '1px solid #000' }}>
+              <Component {...pageProps} />
+            </main>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <aside style={{ border: '1px solid #633' }}>
+              <p>testAside</p>
+            </aside>
+          </GridItem>
+        </Grid>
+      </Container>
+      <footer>testFooter</footer>
     </ChakraProvider>
   )
 }
