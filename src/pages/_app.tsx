c28759176cb3ee059deea45d33e73f25f725eafe
妹尾弦 <gengineer.web@gmail.com>
2020-12-18 22:37:45 +0900

    🎨 emotion-resetの適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,27 +4,36 @@ import { AppProps } from 'next/app'
 import { ChakraProvider, Grid, GridItem, Container } from '@chakra-ui/react'
 import { TheHeader } from '@/components/common/organisms'
 import { baseW } from '@/utils/common'
+import reset from 'emotion-reset'
+import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
-      <TheHeader />
-      <Container maxW={baseW}>
-        <Grid templateColumns='repeat(8, 1fr)' gap={4}>
-          <GridItem colSpan={6}>
-            <main style={{ border: '1px solid #000' }}>
-              <Component {...pageProps} />
-            </main>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <aside style={{ border: '1px solid #633' }}>
-              <p>testAside</p>
-            </aside>
-          </GridItem>
-        </Grid>
-      </Container>
-      <footer>testFooter</footer>
-    </ChakraProvider>
+    <>
+      <Global
+        styles={css`
+          ${reset}
+        `}
+      />
+      <ChakraProvider>
+        <TheHeader />
+        <Container maxW={baseW}>
+          <Grid templateColumns='repeat(8, 1fr)' gap={4}>
+            <GridItem colSpan={6}>
+              <main style={{ border: '1px solid #000' }}>
+                <Component {...pageProps} />
+              </main>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <aside style={{ border: '1px solid #633' }}>
+                <p>testAside</p>
+              </aside>
+            </GridItem>
+          </Grid>
+        </Container>
+        <footer>testFooter</footer>
+      </ChakraProvider>
+    </>
   )
 }
 
