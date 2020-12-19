257dad1322bab3a25200806fa79b796fad8dea49
妹尾弦 <gengineer.web@gmail.com>
2020-12-19 23:20:28 +0900

    🚑 型誤りを修正

--- src/components/article/organisms/ArticleBody.tsx
+++ src/components/article/organisms/ArticleBody.tsx
@@ -11,7 +11,7 @@ export const ArticleBody = ({ article }: Props): JSX.Element => (
     <h1>{article.title}</h1>
     <p>{article.publishedAt}</p>
     <p>{article.category.name}</p>
-    {article.tag?.map((tag) => (
+    {article.tags?.map((tag) => (
       <p>{tag.name}</p>
     ))}
     <div dangerouslySetInnerHTML={{ __html: `${article.body}` }} />
