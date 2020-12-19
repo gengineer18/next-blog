c947e25a33d51c0c4c8b464bf51d3ee8f6a9b069
妹尾弦 <gengineer.web@gmail.com>
2020-12-19 23:17:00 +0900

    💄 レイアウト修正

--- src/components/home/organisms/ArticleList.tsx
+++ src/components/home/organisms/ArticleList.tsx
@@ -12,13 +12,15 @@ export const ArticleList = ({ articles }: Props): JSX.Element => (
   <ul>
     <Grid templateColumns='repeat(2, 1fr)' gap={4}>
       {articles.map((article) => (
-        <li key={article.id}>
-          <Link href={`/articles/${article.id}`}>
-            <GridItem colSpan={1}>
-              <ArticleListItem article={article} />
-            </GridItem>
-          </Link>
-        </li>
+        <GridItem colSpan={[2, 2, 1]} key={article.id}>
+          <li>
+            <Link href={`/articles/${article.id}`}>
+              <a>
+                <ArticleListItem article={article} />
+              </a>
+            </Link>
+          </li>
+        </GridItem>
       ))}
     </Grid>
   </ul>
