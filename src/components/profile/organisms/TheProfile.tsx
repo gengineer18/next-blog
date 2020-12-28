2433880f1ef7be06efde5813e135ae73da7acdee
妹尾弦 <gengineer.web@gmail.com>
2020-12-28 23:22:14 +0900

    💄 見た目を修正

--- src/components/profile/organisms/TheProfile.tsx
+++ src/components/profile/organisms/TheProfile.tsx
@@ -1,8 +1,9 @@
 import { TProfile } from '@/types'
 import React from 'react'
-import { Box } from '@chakra-ui/react'
-import { ArticleTitle } from '@/components/common/article/atoms'
+import { Box, Center } from '@chakra-ui/react'
+import { ArticleTitle, ArticleImage } from '@/components/common/article/atoms'
 import { ArticleBody } from '@/components/article/atoms'
+import { ArticleContentsBox } from '@/components/article/molecules'
 
 type Props = {
   profile: TProfile
@@ -10,8 +11,19 @@ type Props = {
 
 export const TheProfile = ({ profile }: Props): JSX.Element => (
   <article>
+    {profile.mainImage && (
+      <Center mb={6}>
+        <ArticleImage imageUrl={profile.mainImage.url} width={500} height={250} />
+      </Center>
+    )}
     <Box mb={2}>
       <ArticleTitle title={profile.title} size='xl' />
+    </Box>
+    <Box mb={2}>
+      <ArticleBody body={profile.intro} />
+    </Box>
+    <Box mb={8}>
+      <ArticleContentsBox contents={profile.contents} />
     </Box>
     <ArticleBody body={profile.body} />
   </article>
--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -2,7 +2,13 @@ import { TCmsResponse } from '@/types'
 
 type BaseProfile = {
   title: string
+  mainImage?: {
+    url: string
+  }
+  contents: string
+  intro: string
   body: string
+  description: string
 }
 
 export type TProfile = TCmsResponse & BaseProfile
