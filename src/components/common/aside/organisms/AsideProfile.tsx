28a0d3393120c04d1c3dd34610a7d612dd3f2aa7
妹尾弦 <gengineer.web@gmail.com>
2020-12-28 23:06:23 +0900

    ✨ プロフィールへの導線を作成

--- src/components/common/aside/organisms/AsideProfile.tsx
+++ src/components/common/aside/organisms/AsideProfile.tsx
@@ -1,4 +1,16 @@
+import { Box, Button, Flex } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
 import { AsideBox } from '../molecules'
 
-export const AsideProfile: React.FC = () => <AsideBox title='プロフィール'>Profile</AsideBox>
+export const AsideProfile: React.FC = () => (
+  <AsideBox title='プロフィール'>
+    <Box>Profile</Box>
+
+    <Flex justifyContent='center'>
+      <Link href='/profile'>
+        <Button fontSize='sm'>プロフィールはこちらから</Button>
+      </Link>
+    </Flex>
+  </AsideBox>
+)
