221e25edd1d7a30d689e54aadce9ba8102e884f5
妹尾弦 <gengineer.web@gmail.com>
2020-12-31 10:38:35 +0900

    🚸OGP設定

---
+++ src/utils/ogp.ts
@@ -0,0 +1,9 @@
+export const ogTitle = 'Sorellina Coda Official Blog'
+export const ogDescription =
+  'Sorellina Coda(代表: 妹尾 弦)のブログです。フロントエンドにまつわるあれこれ、フリーランスとしての働き方、マネーリテラシーなどの生活をより豊かにするTipsなどを書いていきます。'
+export const ogSiteName = 'Sorellina Coda Official Blog'
+export const ogType = { web: 'website', article: 'article' }
+export const ogUrl = 'https://blog-sorellina-coda.dev'
+export const twCard = { summary: 'summary', lgImage: 'summary_large_image' }
+export const twSite = '@gengineer18'
+export const ogImage = `${ogUrl}/ogp.png`
