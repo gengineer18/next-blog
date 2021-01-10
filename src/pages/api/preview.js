2c8b29b6516a025415ded2d64d131fc045f4776c
妹尾弦 <gengineer.web@gmail.com>
2021-01-10 12:12:59 +0900

    🚸 previewの作成

---
+++ src/pages/api/preview.js
@@ -0,0 +1,27 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import fetch from 'node-fetch'
+import { apiKey } from '@/utils/common'
+
+export default async (req, res) => {
+  if (!req.query.id) {
+    return res.status(404).end()
+  }
+  const content = await fetch(
+    `${process.env.API_PATH}/blog/${req.query.id}?fields=id&draftKey=${req.query.draftKey}`,
+    apiKey
+  )
+    .then((res) => res.json())
+    .catch((_error) => null)
+
+  if (!content) {
+    return res.status(401).json({ message: 'invalid slug' })
+  }
+
+  res.setPreviewData({
+    id: content.id,
+    draftKey: req.query.draftKey,
+  })
+  res.writeHead(307, { Location: `/articles/${content.id}` })
+  res.end('Preview Mode Enabled')
+}
